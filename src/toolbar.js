003909a1b67b6fbbaac83dfe6390437e48b716f5
Menard, Alexis <alexis.menard@intel.com>
2022-12-05 15:41:21 -0500

    Wire the new stylus customizations panel.

--- src/toolbar.js
+++ src/toolbar.js
@@ -289,6 +289,15 @@ export class DrawingToolbar extends LitElement {
             height: 50px;
             margin-top: 10px;
         }
+
+        #read-stylus-panel {
+            display: none;
+            grid-row: span 3;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
     `;
 
     static get properties() {
@@ -296,6 +305,9 @@ export class DrawingToolbar extends LitElement {
             currentLineStyle : { type: String, reflectToAttribute: true, attribute: true },
             currentLineColor : { type: String, reflectToAttribute: true, attribute: true },
             currentLineWidth : { type: Number, reflectToAttribute: true, attribute: true },
+            preferredStyle : { type: String, reflectToAttribute: true, attribute: true },
+            preferredColor : { type: String, reflectToAttribute: true, attribute: true },
+            preferredWidth : { type: Number, reflectToAttribute: true, attribute: true },
         };
     }
 
@@ -310,6 +322,7 @@ export class DrawingToolbar extends LitElement {
             console.log('Pen Customizations are not supported.');
             this._penCustomizationsButton.style.display = 'none';
         }
+        this._readStylusPanel = this.shadowRoot.querySelector('#read-stylus-panel');
     }
 
     _styleSelected(style) {
@@ -347,7 +360,8 @@ export class DrawingToolbar extends LitElement {
         let event = new CustomEvent('lineColor-changed', {
             detail: { lineColor: color},
             bubbles: true,
-            composed: true });
+            composed: true
+        });
         this.dispatchEvent(event);
         this.requestUpdate('currentLineColor', oldColor);
     }
@@ -375,12 +389,55 @@ export class DrawingToolbar extends LitElement {
         let event = new CustomEvent('lineWidth-changed', {
             detail: { lineWidth: width},
             bubbles: true,
-            composed: true });
+            composed: true
+        });
         this.dispatchEvent(event);
         this.requestUpdate('currentLineWidth', oldWidth);
     }
     
     get currentLineWidth() { return this._currentLineWidth; }
+
+    set preferredColor(color) {
+        let oldColor = this._preferredColor;
+        this._preferredColor = color;
+        let event = new CustomEvent('preferredColor-changed', {
+            detail: { preferredColor: color},
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(event);
+        this.requestUpdate('preferredColor', oldColor);
+    }
+
+    get preferredColor() { return this._preferredColor; }
+
+    set preferredStyle(style) {
+        let oldStyle = this._preferredStyle;
+        this._preferredStyle = style;
+        let event = new CustomEvent('preferredStyle-changed', {
+            detail: { preferredStyle: style},
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(event);
+        this.requestUpdate('preferredStyle', oldStyle);
+    }
+
+    get preferredStyle() { return this._preferredStyle; }
+
+    set preferredWidth(width) {
+        let oldWidth = this._preferredWidth;
+        this._preferredWidth = width;
+        let event = new CustomEvent('preferredWidth-changed', {
+            detail: { preferredWidth: width},
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(event);
+        this.requestUpdate('preferredWidth', oldWidth);
+    }
+
+    get preferredWidth() { return this._preferredWidth; }
 
     _lineWidthChanged() {
         this.currentLineWidth = this._lineWidthRange.value;
@@ -391,11 +448,136 @@ export class DrawingToolbar extends LitElement {
         return (typeof event.penCustomizationsDetails !== 'undefined');
     }
 
+    _readAndSetPreferredColor = async (event) => {
+        try {
+            this.preferredColor = await event.penCustomizationsDetails.getPreferredInkingColor();
+            this.currentLineColor = this.preferredColor;
+        } catch (error) {
+            this._emitErrorToast('Couldn\'t read the preferred color from the stylus.');
+        }
+    }
+
+    _readAndSetPreferredStyle = async (event) => {
+        try {
+            this.preferredStyle = await event.penCustomizationsDetails.getPreferredInkingStyle();
+            this.preferredStyle = this.preferredStyle.toLowerCase();
+            this.currentLineStyle = this.preferredStyle;
+        } catch (error) {
+            this._emitErrorToast('Couldn\'t read the preferred style from the stylus.');
+        }
+    }
+
+    _readAndSetPreferredWidth = async (event) => {
+        try {
+            this.preferredWidth = await event.penCustomizationsDetails.getPreferredInkingWidth();
+            this.currentLineWidth = this.preferredWidth;
+        } catch (error) {
+            this._emitErrorToast('Couldn\'t read the preferred width from the stylus.');
+        }
+    }
+
+    _writePreferredColor = async (event) => {
+        try {
+            this.preferredColor = await event.penCustomizationsDetails.setPreferredInkingColor(this.currentLineColor);
+            this._emitSuccessToast('The current color was written successfully.');
+        } catch (error) {
+            console.log(error)
+            this._emitErrorToast('The current color could not be written on the stylus.');
+        }
+    }
+
+    _writePreferredWidth = async (event) => {
+        try {
+            this.preferredWidth = await event.penCustomizationsDetails.setPreferredInkingWidth(this.currentLineWidth);
+            this._emitSuccessToast('The current width was written successfully.');
+        } catch (error) {
+            this._emitErrorToast('The current width could not be written on the stylus.');
+        }
+    }
+
+    _writePreferredStyle = async (event) => {
+        try {
+            let preferredStyle = await event.penCustomizationsDetails.setPreferredInkingStyle(this.currentLineStyle.toUpperCase());
+            this.preferredStyle = preferredStyle.toLowerCase();
+            this._emitSuccessToast('The current style was written successfully.');
+        } catch (error) {
+            this._emitErrorToast('The current style could not be written on the stylus.');
+        }
+    }
+
+    // Always escape HTML for text arguments!
+    _escapeHtml(html) {
+        const div = document.createElement('div');
+        div.textContent = html;
+        return div.innerHTML;
+    }
+
+    _createToastNotification(message, variant = 'success', icon = 'check2-circle', duration = 4000) {
+        const alert = Object.assign(document.createElement('sl-alert'), {
+            variant,
+            closable: true,
+            duration: duration,
+            innerHTML: `
+                <sl-icon name="${icon}" slot="icon"></sl-icon>
+                ${this._escapeHtml(message)}
+            `
+        });
+        this.shadowRoot.append(alert);
+        return alert.toast();
+    }
+
+    _emitSuccessToast(message) {
+        this._createToastNotification(message);
+    }
+
+    _emitErrorToast(message) {
+        this._createToastNotification(
+            message,
+            'danger',
+            'exclamation-octagon');
+    }
+
+    _readStylusCustomizations = async (event, shouldShowError = false) => {
+        // Let's read the features before showing the panel.
+        let preferredCells = this.shadowRoot.querySelectorAll('.preferred');
+        try {
+            const preferredColor = await event.penCustomizationsDetails.getPreferredInkingColor();
+            const preferredStyle = await event.penCustomizationsDetails.getPreferredInkingStyle();
+            const preferredWidth = await event.penCustomizationsDetails.getPreferredInkingWidth();
+            // If all the readings succeeded then we can set them.
+            this.preferredColor = preferredColor;
+            this.preferredStyle = preferredStyle.toLowerCase();
+            this.preferredWidth = preferredWidth;
+            this._readStylusPanel.style.display = 'none';
+            preferredCells.forEach((element) => {
+                element.style.display = 'flex';
+            });
+        } catch (error) {
+            // We couldn't read the attributes, let's show a panel with a button
+            // to let the user try again.
+            this._readStylusPanel.style.display = 'flex';
+            preferredCells.forEach((element) => {
+                element.style.display = 'none';
+            });
+            if (shouldShowError) {
+                this._emitErrorToast('Could not read the customizations from the stylus.');
+            }
+        }
+    }
+
+    _showStylusCustomizationsDrawer = async (event) => {
+        this._readStylusCustomizations(event, false);
+        this._drawer.show();
+    }
+
     constructor() {
         super();
         this._currentLineStyle = 'pencil';
         this._currentLineColor = '#000000';
         this._currentLineWidth = 1;
+        this._preferredWidth = 1;
+        this._preferredColor = '#000000';
+        this._preferredStyle = 'pencil';
         registerIconLibrary('my-icons', {
             resolver: name => `icons/${name}.svg`,
             mutator: svg => svg.setAttribute('fill', 'currentColor')
@@ -439,13 +621,13 @@ export class DrawingToolbar extends LitElement {
                         <sl-range id="line-width-range" min="1" max="50" 
                             @sl-change="${() => this._lineWidthChanged()}">
                         </sl-range>
-                        Current width: ${this._currentLineWidth} px
+                        Current width: ${this.currentLineWidth} px
                     </div>
                 </sl-dropdown>
                 <div class="expand"></div>
                 <drawing-button id="pen-customizations-button"
                     library="my-icons" name="pen_customizations"
-                    @pointerdown="${() => this._drawer.show()}">
+                    @pointerdown="${(event) => this._showStylusCustomizationsDrawer(event)}">
                 </drawing-button>
                 <sl-drawer label="Stylus Customizations" id="drawer" placement="start">
                     <div class="drawer-content">
@@ -455,18 +637,28 @@ export class DrawingToolbar extends LitElement {
                             <div class="title border border-top">Stylus</div>
                             <div class="title"></div>
                             <div class="title border border-top">Editor</div>
-                            <div class="category border">
+                            <div id="read-stylus-panel" class="border border-bottom">
+                                Read preferences from stylus
+                                <sl-tooltip content="Read preferences from stylus">
+                                    <sl-button class="button">
+                                        <sl-icon
+                                            name="arrow-clockwise" label="Read from stylus"
+                                            @pointerdown="${(event) => this._readStylusCustomizations(event, true)}"></sl-icon>
+                                    </sl-button>
+                                </sl-tooltip>
+                            </div>
+                            <div class="category border preferred">
                                 <div class="category-title">Preferred Color</div>
-                                <div class="color-box" style="background-color: ${this.currentLineColor}"></div>
+                                <div class="color-box" style="background-color: ${this._preferredColor}"></div>
                             </div>
                             <div class="category">
                                 <sl-tooltip content="Send preferred color to stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._writePreferredColor}">
                                         <sl-icon library="my-icons" name="left-arrow" label="Send preferred color to stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
                                 <sl-tooltip content="Read preferred color from stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._readAndSetPreferredColor}">
                                         <sl-icon library="my-icons" name="right-arrow" label="Read preferred color from stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
@@ -475,18 +667,18 @@ export class DrawingToolbar extends LitElement {
                                 <div class="category-title">Drawing Color</div>
                                 <div class="color-box" style="background-color: ${this.currentLineColor}"></div>
                             </div>
-                            <div class="category border">
+                            <div class="category border preferred">
                                 <div class="category-title">Preferred Width</div>
-                                <div class="value">3px</div>
+                                <div class="value">${this._preferredWidth} px</div>
                             </div>
                             <div class="category">
                                 <sl-tooltip content="Send preferred width to stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._writePreferredWidth}">
                                         <sl-icon library="my-icons" name="left-arrow" label="Send preferred width to stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
                                 <sl-tooltip content="Read preferred width from stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._readAndSetPreferredWidth}">
                                         <sl-icon library="my-icons" name="right-arrow" label="Read preferred width from stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
@@ -495,25 +687,25 @@ export class DrawingToolbar extends LitElement {
                                 <div class="category-title">Drawing Width</div>
                                 <div class="value">${this.currentLineWidth} px</div>
                             </div>
-                            <div class="category border border-bottom">
+                            <div class="category border border-bottom preferred">
                                 <div class="category-title">Preferred Style</div>
-                                <div class="value">${this.currentTool}</div>
+                                <div class="value">${this._preferredStyle}</div>
                             </div>
                             <div class="category">
                                 <sl-tooltip content="Send preferred style to stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._writePreferredStyle}">
                                         <sl-icon library="my-icons" name="left-arrow" label="Send preferred style to stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
                                 <sl-tooltip content="Read preferred style from stylus">
-                                    <sl-button class="button">
+                                    <sl-button class="button" @pointerdown="${this._readAndSetPreferredStyle}">
                                         <sl-icon library="my-icons" name="right-arrow" label="Read preferred style from stylus"></sl-icon>
                                     </sl-button>
                                 </sl-tooltip>
                             </div>
                             <div class="category border border-bottom">
                                 <div class="category-title">Drawing Style</div>
-                                <div class="value">${this.currentTool}</div>
+                                <div class="value">${this.currentLineStyle}</div>
                             </div>
                         </div>
                         <sl-divider></sl-divider>
